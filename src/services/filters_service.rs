196d8820ac88a8ce9df292cf0304f1bb9cfc3907
Terje A. Bergesen <terje.bergesen@stormgeo.com>
2024-06-17 15:30:37 +0200

    fix(cleanup): removed unused things

--- src/services/filters_service.rs
+++ src/services/filters_service.rs
@@ -3,33 +3,29 @@ use anyhow::Error;
 use serde::{Deserialize, Serialize};
 use std::num::ParseIntError;
 use std::{
-  collections::HashMap,
-  sync::{Arc, Mutex},
+    collections::HashMap,
+    sync::{Arc, Mutex},
 };
 
 #[derive(Clone)]
 pub struct FiltersService {
-  filters_repo: FiltersRepo,
-  filters_store: Arc<Mutex<Option<Filters>>>,
+    filters_repo: FiltersRepo,
 }
 
 impl FiltersService {
-  pub async fn new(repo: FiltersRepo) -> Result<Self, ()> {
-    Ok(Self {
-      filters_repo: repo,
-      filters_store: Arc::default(),
-    })
-  }
+    pub async fn new(repo: FiltersRepo) -> Result<Self, ()> {
+        Ok(Self { filters_repo: repo })
+    }
 }
 
 impl FiltersService {
-  pub async fn get_filters(&self) -> Result<Filters, Error> {
-    println!("FiltersService::get_filters");
-    let filters = reqwest::get("https://fps-api.stormgeo.com/fpsapi/v1/fas/filters.json")
-      .await?
-      .json::<Filters>()
-      .await?;
+    pub async fn get_filters(&self) -> Result<Filters, Error> {
+        println!("FiltersService::get_filters");
+        let filters = reqwest::get("https://fps-api.stormgeo.com/fpsapi/v1/fas/filters.json")
+            .await?
+            .json::<Filters>()
+            .await?;
 
-    Ok(filters)
-  }
+        Ok(filters)
+    }
 }
